be380a67741787f99e427da48a1f5cfa411ea559
kapibarakapi565 <kapibarakapi565@gmail.com>
2025-02-21 22:41:51 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :tweets, only: :index 
+  root to: 'tweets#index'
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update] 
   # get "up" => "rails/health#show", as: :rails_health_check
 end
